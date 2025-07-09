361aec188a5718687462af72e2c863cadf95242a
Syed Abdul Ali Shah <124284042+shah541-g@users.noreply.github.com>
2025-07-09 15:49:14 +0500

    Update firebaseConfig.js

--- backend/src/FireBase/firebaseConfig.js
+++ backend/src/FireBase/firebaseConfig.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyC_jVrZo4xDRinwVgfUGr3iHkV9Naiu154",
-  authDomain: "promstack.firebaseapp.com",
-  projectId: "promstack",
-  storageBucket: "promstack.firebasestorage.app",
-  messagingSenderId: "231443991563",
-  appId: "1:231443991563:web:28055de9840cda4724cb0e",
-  measurementId: "G-8SCH170X60"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
